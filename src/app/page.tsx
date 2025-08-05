46e025a0c13a8513dddf358dbf771a23b8b8ecf1
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-08-04 21:01:38 -0300

    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button className="bg-accent px-1.5">Click me</Button>
+      <Button>Click me</Button>
     </div>
   );
 }
